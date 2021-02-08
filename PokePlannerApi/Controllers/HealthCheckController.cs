6090e682e2368c374aae5803cafebb7b40dfe2fc
Julian Lawrance <julian.lawrance@gmail.com>
2021-02-08 00:20:12 +0000

    Add new health check controller so service can be contacted

---
+++ PokePlannerApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PokePlannerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public string HealthCheck()
+        {
+            return "Service is healthy!";
+        }
+    }
+}
